0c29b7a65e205a7e2591f7cb80b6f7f64b1c1fab
renato97 <renato.oliveira83@outlook.com>
2022-08-04 14:57:53 -0300

    chore: add web pack config

---
+++ webpack.config.ts
@@ -0,0 +1,47 @@
+import path from "path"
+import HtmlWebpackPlugin from "html-webpack-plugin"
+import * as webpackDevServer from 'webpack-dev-server'
+import { Configuration } from "webpack"
+
+const config: Configuration = {
+  entry: "./src/index.tsx",
+  mode: 'development',
+  output: {
+    path: path.resolve(__dirname, './dist'),
+    filename: 'index_bundle.js',
+  },
+  target: 'web',
+  devServer: {
+    port: '5000',
+    static: {
+      directory: path.join(__dirname, 'public')
+    },
+    open: true,
+    hot: true,
+    liveReload: true,
+  },
+  resolve: {
+    extensions: [".ts", ".js", ".tsx", ".jsx"],
+  },
+  module: {
+    rules: [
+      {
+        test: /\.(ts|js|tsx|jsx)?$/,
+        exclude: /node_modules/,
+        use: {
+          loader: "babel-loader",
+          options: {
+            presets: ["@babel/preset-env", "@babel/preset-typescript", "@babel/preset-react"],
+          },
+        },
+      },
+    ],
+  },
+  plugins: [
+    new HtmlWebpackPlugin({
+      template: path.join(__dirname, 'public', 'index.html')
+    })
+  ]
+};
+
+export default config
