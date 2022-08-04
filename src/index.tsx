6901928232a4cec2eda90a72cba0042db63b1623
renato97 <renato.oliveira83@outlook.com>
2022-08-04 14:04:58 -0300

    feat: add react files

---
+++ src/index.tsx
@@ -0,0 +1,6 @@
+import React from 'react'
+import  { render }  from 'react-dom'
+import App from './App.js'
+
+const container = document.getElementById('root') as HTMLElement
+render(<App/>, container)
